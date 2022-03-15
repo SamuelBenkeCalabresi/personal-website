33d18b9eae614b44602990be0892bf7495238f71
Samuel Benke Calabresi <samuelbenkec@gmail.com>
2022-03-15 22:04:29 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import remarkFrontmatter from "remark-frontmatter";
+
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@mdx-js/loader",
+          options: {
+            providerImportSource: "@mdx-js/react",
+            remarkPlugins: [remarkFrontmatter],
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+  /*  images: {
+    loader: "imgix",
+    path: "https://images.unsplash.com/",
+  }, */
+};
+
+export default nextConfig;
